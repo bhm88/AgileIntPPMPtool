d12b99cd56fe68e57c4841299c36b110fba00b93
Bharat Kumar Hiremath <Bharat.Hiremath@gds.ey.com>
2020-03-03 14:37:28 +0530

    first commit

---
+++ pmptool/src/main/java/com/fl/p1/pmptool/PmptoolApplication.java
@@ -0,0 +1,13 @@
+package com.fl.p1.pmptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmptoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmptoolApplication.class, args);
+	}
+
+}
---
+++ pmptool/src/test/java/com/fl/p1/pmptool/PmptoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fl.p1.pmptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmptoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
